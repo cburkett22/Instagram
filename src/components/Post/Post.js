da91c08d9e988c3d1b60069746b76c07a3199c83
Corey Burkett <coreyburkett22@gmail.com>
2020-12-31 10:04:10 -0800

    Completed comment functionality with firebase

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Post.css';
+import firebase from 'firebase';
 import Avatar from '@material-ui/core/Avatar';
+import { db } from '../../firebase';
 
-function Post({ username, caption, imageUrl }) {
+function Post({ postId, user, username, caption, imageUrl }) {
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
+
+    useEffect(() => {
+        let unsubscribe;
+        if (postId) {
+            unsubscribe = db
+                .collection("posts")
+                .doc(postId)
+                .collection("comments")
+                .orderBy("timestamp", "asc")
+                .onSnapshot((snapshot) => {
+                    setComments(snapshot.docs.map((doc) => doc.data()))
+                });
+        }
+
+        return (() => {
+            unsubscribe();
+        });
+    }, [postId]);
+
+    const postComment = (event) => {
+        event.preventDefault();
+
+        db.collection("posts").doc(postId).collection("comments").add({
+            text: comment,
+            username: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        });
+        setComment('');
+    };
+
     return (
         <div className="post">
             <div className="post__header">
@@ -14,17 +48,47 @@ function Post({ username, caption, imageUrl }) {
                 <h3>{username}</h3>
             </div>
 
-            {/* header -> avatar + username */}
-
             <img
                 className="post__image"
                 src={imageUrl}
                 alt="User avatar"
             />
+            <p className="post__text">
+                <strong>{username} </strong>
+                {caption}
+            </p>
 
-            <p className="post__text"><strong>{username}</strong> {caption}</p>
+            <div className="post__comments">
+                {comments.map((comment) => (
+                    <p>
+                        <strong>{comment.username}</strong> {comment.text}
+                    </p>
+                ))}
+            </div>
+
+            {user? (
+                <form className="post__commentBox">
+                    <input
+                        className="post__input"
+                        type="text"
+                        placeholder="Add a comment..."
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                    />
+                    <button
+                        className="post__button"
+                        disabled={!comment}
+                        type="submit"
+                        onClick={postComment}
+                    >
+                        Post
+                    </button>
+                </form>
+            ): (
+                <p style={{display: 'flex',  justifyContent:'center', alignItems:'center', paddingBottom:'10px'}}>Sorry... You need to login to comment on a post!</p>
+            )}
         </div>
     )
 }
 
-export default Post
+export default Post;
